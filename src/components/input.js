4db5f6bbafef25eb406f158a70134a719e0f2d1b
Shivg1217 <guptashivnarayan03@gmail.com>
2022-01-16 16:01:05 +0530

    Latest commit for Resetting the entered Value

---
+++ src/components/input.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+function InputField() {
+    // State to store value from the input field
+    const [inputValue, setInputValue] = useState("");
+  
+    // Input Field handler
+    const handleUserInput = (e) => {
+      setInputValue(e.target.value);
+    };
+  
+    // Reset Input Field handler
+    const resetInputField = () => {
+      setInputValue("");
+    };
+  
+    return (
+      <div>
+        {/* Input Field */}
+        <input type="text" value={inputValue} onChange={handleUserInput} />
+  
+        {/* Reset button */}
+        <button onClick={resetInputField}>Reset</button>
+      </div>
+    );
+  }
+
+  export default InputField;
